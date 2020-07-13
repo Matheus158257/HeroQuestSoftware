5c1740ea29f2d895b2d557c05095b88bcaf9bd5e
Matheus158257 <matheus.sasso17@gmail.com>
2020-07-13 20:59:52 -0300

    changes

--- src/br/unicamp/Map/GroupElements/Corner.java
+++ src/br/unicamp/Map/GroupElements/Corner.java
@@ -5,7 +5,7 @@ public class Corner extends GroupMapElement {
 	private int[] adjacentIDs;
 
 	public Corner(int sizeX, int sizeY, int x0, int y0, int ID, int[] adjacentIDs) {
-		super(sizeX, sizeY, x0, y0, ID);
+		super(sizeX, sizeY, x0, y0);
 		// TODO Auto-generated constructor stub
 		this.adjacentIDs = adjacentIDs;
 	}
